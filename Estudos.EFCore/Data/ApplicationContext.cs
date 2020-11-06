5092a449e5d66155562aadd013b6359c4c344ea5
Lizandra <lizandra.oliveirasouza@hotmail.com>
2020-11-06 19:45:37 -0300

    inserindo dados em massa no bd

--- Estudos.EFCore/Data/ApplicationContext.cs
+++ Estudos.EFCore/Data/ApplicationContext.cs
@@ -1,17 +1,24 @@
 using Estudos.EFCore.Data.Configurations;
 using Estudos.EFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Estudos.EFCore.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
       
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=NerdStoreEnterprise;User Id=sa;Password=!123Senha;MultipleActiveResultSets=true");
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Server=localhost,1433;Database=NerdStoreEnterprise;User Id=sa;Password=!123Senha;MultipleActiveResultSets=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Estudos.EFCore/Program.cs
+++ Estudos.EFCore/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+using Estudos.EFCore.Data;
+using Estudos.EFCore.Domain;
+using Estudos.EFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Estudos.EFCore
 {
@@ -6,7 +12,67 @@ namespace Estudos.EFCore
     {
         static void Main(string[] args)
         {
+            using var db = new Data.ApplicationContext();
+            // db.Database.Migrate();
+
+            //verificar se existe migracoes pendentes.
+            // var existe = db.Database.GetPendingMigrations().Any();
+            //
+            // if(existe){}
             Console.WriteLine("Hello World!");
+
+            InserirDadosEmMassa();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto teste",
+                CodigoDeBarras = "14526941",
+                Valor = 10m,
+                TipoProduto = TipoProduto.Embalagem,
+                Ativo = true
+            };
+            
+            using var db = new Data.ApplicationContext();
+            //varias formas de add prodto ao BD.
+            
+            // db.Produtos.Add(produto);
+            db.Set<Produto>().Add(produto);
+            // db.Entry(produto).State = EntityState.Added;
+            // db.Add(produto);
+
+            var registros = db.SaveChanges();
+
+            Console.WriteLine("Total de registros afetados: " + registros);
+        }
+        
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto teste",
+                CodigoDeBarras = "14526941",
+                Valor = 10m,
+                TipoProduto = TipoProduto.Embalagem,
+                Ativo = true
+            };
+
+
+            var cliente = new Cliente
+            {
+                Nome = "Cliente teste",
+                CEP = "08081570",
+                Cidade = "Sao paulo",
+                Estado = "SP",
+                Telefone = "2516-5246"
+            };
+            using var db = new ApplicationContext();
+            db.AddRange(produto,cliente);
+            var registro = db.SaveChanges();
+            Console.WriteLine("Dados: " + registro);
+            
         }
     }
 }
